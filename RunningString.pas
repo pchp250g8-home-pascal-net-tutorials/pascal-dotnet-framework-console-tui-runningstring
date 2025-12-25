01a52115a5d6d798ad6188c9da0b5bb91d0e65c9
Alexander <pchp250g8@tutamail.com>
2025-12-25 13:28:54 +0300

    Created RunningString Project

---
+++ RunningString.pas
@@ -0,0 +1,20 @@
+﻿uses
+  System,
+  System.Threading;
+begin
+  Console.Clear();
+	Console.CursorVisible := False;
+	var strSpaces := new String(#32, 80);
+	var strLine := 'Hello World!!! Running String!!! Press Any Key To Exit!!!';
+	strLine := strSpaces + strLine + strSpaces;
+	var nCounter := 0;
+	var nStrLen := strLine.Length - 80;
+	repeat
+		strSpaces := strLine.Substring(nCounter, 80);
+		Console.SetCursorPosition(10, 10);
+		Console.Write(strSpaces);
+		Thread.Sleep(140);
+		inc(nCounter, 1);
+		If nCounter >= nStrLen then nCounter := 0;
+	until Console.KeyAvailable;
+end.
